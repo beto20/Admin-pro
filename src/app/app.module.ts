af5ef88f5aff1d44ae3ceb01355547f7d37aab48
beto20 <alberto.vs@outlook.com>
2023-09-16 23:46:32 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NopagefoundComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
